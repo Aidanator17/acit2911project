21cf4a4e7e090ca085230791cff0cc27fca79ca9
Peter Gibbons <gibbons.peter312@gmail.com>
2021-05-05 12:01:11 -0700

    created the player
    created the player so you can now play against the AI. Working on some piece detection in order to create a cleaner UI

--- game.py
+++ game.py
@@ -9,12 +9,30 @@ from chessboard import display
 # also it is possibble to undo and redo moves and tracks repeating states
 #pygame.display.set_caption("Chess Game")
 
-def player(board):
-    #thinking still 
-    return move_code
+def user(game_board):
+    display.update(game_board.fen())
+    uci = get_move("%s's move [q to quite]> " % who(game_board.turn))
+    legal_uci_moves = [move.uci() for move in game_board.legal_moves]
+    while uci not in legal_uci_moves:
+        print("Legal moves: " + (",".join(sorted(legal_uci_moves))))
+        uci = get_move("%s's move[q to quit]> " % who(game_board.turn))
+    return uci
+
+def get_move(prompt):
+    uci = input(prompt)
+    if uci and uci[0] == "q":
+        raise KeyboardInterrupt()
+    try:
+        chess.Move.from_uci(uci)
+    except:
+        uci = None
+    return uci
 
 def random_player(game_board):
     move = random.choice(list(game_board.legal_moves))
+    # if chess.BaseBoard():
+    #     chess.BaseBoard.piece_at("f7")   # Working on displaying the peice that was moved wants self to be passed through but that causes error
+    print("AI Move: ",move)
     return move.uci()
 
 def who(player):
@@ -23,7 +41,10 @@ def who(player):
 
 def view_game(game_board, use_display):
     """ function for displaying the board """
+    # if bool(game_board.castling_rights):
+    #     print("you can castle: BBH1")
     return display.update(game_board.fen())
+
 def play_game(player1, player2, pause=0.1):
     game_board = chess.Board()
     use_display = display.start(game_board.fen())
@@ -48,6 +69,9 @@ def play_game(player1, player2, pause=0.1):
         msg = "Game interrupted!"
         return (None, msg, game_board)
     result = None
+    if game_board.is_check():
+        msg = "check: " + who(not game_board.turn)
+        print(msg)
     if game_board.is_checkmate():
         msg = "checkmate: " + who(not game_board.turn) + " wins!"
         result = not game_board.turn
@@ -67,6 +91,6 @@ def play_game(player1, player2, pause=0.1):
     if use_display is not None:
         print(msg)
     return (result, msg, game_board)
-play_game(random_player, random_player)
+play_game(user, random_player)
 
 
