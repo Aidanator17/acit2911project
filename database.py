fc0f1e995c620da58122c909db795ad781a01ea7
Aidan Christopher <gandamovies@gmail.com>
2021-05-11 21:14:16 -0700

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,8 @@
 
 from sqlalchemy import Table, Column, String, MetaData, create_engine, ARRAY
 import uuid
-import os
-import re
 
-def dbpush(pn,pc,aic,mov,win,plyd):
+def dbpush(pn,pc,aic,mov,win,plyd,tim):
 
     engine = create_engine("postgresql://iuulkwuvvrdmxw:b53508be72378dc1e867c7b4155e7dab99eaf2ee92a4c5c18cb62bffbb6a0342@ec2-35-174-35-242.compute-1.amazonaws.com:5432/d4m4h0c2mu2o1u", echo=True)
     metadata = MetaData(engine)
@@ -17,12 +15,14 @@ def dbpush(pn,pc,aic,mov,win,plyd):
         Column('AIcolor', String(5)),
         Column('moves', ARRAY(String)),
         Column('winner', String(500)),
-        Column('played', String(500))
+        Column('played', String(500)),
+        Column('time', String(500))
     )
 
-    ins = matches.insert().values(match_id=str(uuid.uuid4()),playername=pn, playercolor=pc, AIcolor=aic, moves=mov, winner=win, played=plyd)
+    ins = matches.insert().values(match_id=str(uuid.uuid4()),playername=pn, playercolor=pc, AIcolor=aic, moves=mov, winner=win, played=plyd, time=tim)
 
     conn = engine.connect()
     conn.execute(ins)
 
-dbpush('Aidan','White','Black',['white pawn from a2 to a4','black knight from b7 to d6'],'Aidan','2021-05-10 5:31PM')
+dbpush('Aidan','White','Black',['white pawn from a2 to a4','black knight from b7 to d6'],'player','2021-05-10 5:31PM','180')
+dbpush('Aidan','Black','White',['white pawn from a2 to a4','black knight from b7 to d6'],'AI','2021-05-11 8:43PM','360')
